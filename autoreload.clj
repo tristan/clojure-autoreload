fbe0f86d8cb52f0c6919bb6c8c95b1af8dda6e5d
Tristan King <tristan@wallaby.(none)>
2010-01-21 11:13:21 +0100

    merged process.clj into autoreload.clj so autoreload code can be used without it being in the root of the project that requires it

--- autoreload.clj
+++ autoreload.clj
@@ -6,17 +6,39 @@
 
 (ns autoreload
   (:use clojure.set
-	(clojure.contrib [duck-streams :only (reader)]))
-  (:require process)
+	(clojure.contrib [duck-streams :only (reader writer)]))
   (:import
+   (java.lang.management ManagementFactory)
    (java.io File)))
+
+(defn get-cmdline []
+  (let [pid (last (first (re-seq #"([0-9]+)@" (.. ManagementFactory getRuntimeMXBean getName))))]
+    (cond (re-find #"Windows" (System/getProperty "os.name"))
+	  (let [p (.exec (Runtime/getRuntime) (str "wmic PROCESS where processid=" pid " get Commandline /value")
+			 (into-array String (for [i (System/getenv)] (str (.getKey i) "=" (.getValue i)))))]
+	    (let [w (writer (.getOutputStream p))]
+	      (.println w "testing")
+	      (.close w))
+	    (let [r (with-open [rdr (reader (.getInputStream p))]
+		      (apply #'str (for [l (line-seq rdr)] l)))] ; have to run str on all so we don't return a lazy-seq.
+	      (.waitFor p)
+	      (last (first (re-seq #"CommandLine=([\s\S]+)" r)))))
+	  ;(re-find #"Unix|Linux" (System/getProperty "os.name")) ; only tested on ubuntu!
+	  :else ; attempt to use unix type
+	  (try
+	   (let [p (.exec (Runtime/getRuntime) (str "ps -p " pid " w -o command --no-headers")
+			  (into-array String (for [i (System/getenv)] (str (.getKey i) "=" (.getValue i)))))]
+	     (let [r (with-open [rdr (reader (.getInputStream p))]
+		       (apply #'str (for [l (line-seq rdr)] l)))]
+	       (.waitFor p)
+	       r))
+	   (catch java.io.IOException e (throw (Exception. "unable to determine operating system to find command")))))))
 
 (defn list-all-clj [#^String dir]
   (for [f (filter #(and (.isFile %) (.endsWith (.. % getName toLowerCase) ".clj")) (file-seq (File. dir)))]
     {:file (.getPath f) :last-modified (.lastModified f)}))
 
-(def cmdline
-     (process/get-cmdline))
+(def cmdline (get-cmdline))
 
 (defn file-watching-thread []
   (loop [files (set (list-all-clj "."))]
